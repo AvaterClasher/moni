e47c6622d0a0b6d4cc8d3a8d8ad2aca69519c1d0
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2023-01-14 13:36:13 +0530

    new file:   .gitignore 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	new file:   src/components/About.jsx 	modified:   src/components/Hero.jsx 	modified:   src/components/Navbar.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
-
+import About from './components/About.jsx';
+import Support from './components/Support.jsx';
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <About />
+      
       
     </>
   );
