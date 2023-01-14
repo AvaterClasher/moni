8c2ba6f54f617c95a37438c2608e150de032f49b
Soumyadip Moni <116944847+AvaterClasher@users.noreply.github.com>
2023-01-14 15:24:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import Hero from './components/Hero.jsx'
 import About from './components/About.jsx';
 import Support from './components/Support.jsx';
 import Footer from './components/Footer.jsx';
-<<<<<<< HEAD
 import AllInOne from './components/AllInOne.jsx';
 import Pricing from './components/Pricing.jsx';
-=======
->>>>>>> 51ed06d2b806ee94f29bbfb0d06b31f22fee8c49
+
 
 function App() {
   return (
@@ -15,15 +13,15 @@ function App() {
       <Navbar />
       <Hero />
       <About />
-<<<<<<< HEAD
+
       <Support />
       <AllInOne />
       <Pricing />
       <Footer />
-=======
+
       <Support/>
       <Footer/>
->>>>>>> 51ed06d2b806ee94f29bbfb0d06b31f22fee8c49
+
       
       
       
